99c2dd96a390032f48f60647db05d8f131eb3d99
车大棒 <che_dabang@qq.com>
2020-04-15 22:11:02 +0800

    本地保存

---
+++ NodeStudy/DEMO_02/test.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const fs = require("fs");
+const hostname = "0.0.0.0";
+const port = 80;
+const server = http.createServer((req, res) => {
+  //   res.statusCode = 200;
+  //   res.setHeader("Content-Type", "text/plain");
+  let templateStr = fs.readFileSync("./index.html").toString();
+  res.end(templateStr);
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
